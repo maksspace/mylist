182ba2903a146ea335a13a33310d27903b929fad
maksspace <maksspaces@mail.ru>
2016-02-02 17:49:02 +0300

    change some macros on function. change typeof

--- list.h
+++ list.h
@@ -2,58 +2,83 @@
 // Doubly linked list implementation by maksspace //
 // ---------------------------------------------- //
 
+#include <stdint.h>
+
 #ifndef maksspace_list_h
 #define maksspace_list_h
 
 #define LIST_NODE(type) \
     struct {            \
-        type data;      \
         size_t next;    \
         size_t prev;    \
+        type data;      \
     }
 
-#define LIST_NODE_TYPE_NEW(type, name) typedef LIST_NODE(type) name
+#define NEXT_NODE_PTR(size_t_name)               *((size_t*)(size_t_name))                                             // return pointer to next node
+#define PREV_NODE_PTR(size_t_name)               *(((size_t*)(size_t_name)) + 1)                                       // return pointer to previous node
+#define LIST_NODE_TYPE_NEW(type, name)           typedef LIST_NODE(type) name                                          // creates a node type "name" with the type data field == "type"
+#define NODE_LINK_INIT(name, next_val, prev_val) {NEXT_NODE_PTR(name) = (next_val); PREV_NODE_PTR(name) = (prev_val);} // initializes node's link
+#define LIST_NODE_VAL(type, node_ptr)            ((type*)(node_ptr))                                                   // return pointer to node type == "type"
+#define LIST_ITER(ptr)                           (size_t)(ptr)                                                         // create list iterator
 
-#define LIST_NODE_LINK_INIT(name) ((name)->next = (name)->prev = 0)
-#define LIST_NODE_NEXT(type, node_ptr) ((type*)((node_ptr)->next))
-#define LIST_NODE_PREV(type, node_ptr) ((type*)((node_ptr)->prev))
-#define LIST_NODE_GET(type, node_ptr) ((type*)(node_ptr))
+// allocate new node and return pointer on him
+void* list_node_new(size_t sizeof_node, void*(node_allocator)(size_t))
+{
+    void* new_node = node_allocator(sizeof_node);
+    if(new_node != NULL)
+        NODE_LINK_INIT(new_node, 0, 0);
+    return new_node;
+}
 
-#define LIST_INSERT(new_node, prev_node, next_node)         \
-    if((next_node) == 0) {                                  \
-        (prev_node)->next = (size_t)(new_node);             \
-        (new_node)->prev = (size_t)(prev_node);             \
-        (new_node)->next = 0;                               \
-    }                                                       \
-    else {                                                  \
-        (next_node)->prev = (size_t)(new_node);             \
-        (new_node)->next  = (size_t)(next_node);            \
-        (new_node)->prev  = (size_t)(prev_node);            \
-        (prev_node)->next = (size_t)(new_node);             \
+// insert new_node betwin prev_node and next_node
+void list_insert(size_t new_node, size_t prev_node, size_t next_node)
+{
+    if(next_node == 0) {
+        NEXT_NODE_PTR(prev_node) = new_node;
+        PREV_NODE_PTR(new_node) = prev_node;
+        NEXT_NODE_PTR(new_node) = 0;
     }
+    else {
+        PREV_NODE_PTR(next_node) = new_node;
+        NEXT_NODE_PTR(new_node)  = next_node;
+        PREV_NODE_PTR(new_node)  = prev_node;
+        NEXT_NODE_PTR(prev_node) = new_node;
+    }
+}
 
-#define LIST_PREPEND(node, head)    \
-    {(head)->prev = (size_t)(node); \
-    (node)->next = (size_t)(head);  \
-    (node)->prev = 0;               \
-    (head) = (node);}
+// insert new_node before head node and return pointer to new head
+size_t list_prepend(size_t new_node_ptr, size_t head_ptr) {
+    PREV_NODE_PTR(head_ptr) = new_node_ptr;
+    NODE_LINK_INIT(new_node_ptr, head_ptr, 0);
+    return new_node_ptr;
+}
 
-#define LIST_ITER(ptr) (size_t)(ptr)
-#define LIST_ITER_DOWN(iter_name, type, head_ptr) iter_name = (size_t)LIST_NODE_NEXT(type, (type*)iter_name)
-#define LIST_ITER_UP(iter_name, type, tail_ptr) iter_name = (size_t)LIST_NODE_PREV(type, (type*)iter_name)
+// return next node by pointer
+size_t list_iter_down(size_t node) {
+    return NEXT_NODE_PTR(node);
+}
 
-#define LIST_NODE_DELETE(from, to) \
-    {(from)->next = (size_t)(to);  \
-    (to)->prev = (size_t)(foo);}
+// return previous node by pointer
+size_t list_iter_up(size_t node) {
+    return PREV_NODE_PTR(node);
+}
 
-#define LIST_DELETE_STATIC(type, head, free_data)            \
-    {size_t current_node = (size_t)(head);                   \
-    size_t next_node = 0;                                    \
-    while(current_node != 0) {                               \
-        next_node = LIST_NODE_GET(type, current_node)->next; \
-        free_data((void*)current_node);                      \
-        current_node = next_node;                            \
-    }}
+// delete nodes betven $from_node and $to_node
+void list_node_delete(size_t from_node, size_t to_node) {
+    NEXT_NODE_PTR(from_node) = to_node;
+    PREV_NODE_PTR(to_node) = from_node;
+}
 
+// delete list, in nodes field "data" was not dynamicly allocated
+void list_delete_static(size_t head, void(*free_data)(void*))
+{
+    size_t current_node = head;
+    size_t next_node = 0;
+    while(current_node != 0) {
+        next_node = NEXT_NODE_PTR(current_node);
+        free_data((void*)current_node);
+        current_node = next_node;
+    }
+}
 
 #endif /* maksspace_list_h */
--- main.c
+++ main.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
 
 #include "list.h"
 
-
 LIST_NODE_TYPE_NEW(int, node_int);
 
 int main(int argc, char *argv[])
 {
-
-    node_int* my_numbers = malloc(sizeof(node_int));
-    LIST_NODE_LINK_INIT(my_numbers);
+    
+    node_int* my_numbers = list_node_new(sizeof(node_int), malloc);
     my_numbers->data = 0;
     
+    unsigned long t = clock();
     for(int i = 1; i < 1000; i++) {
         node_int* tmp = malloc(sizeof(node_int));
         tmp->data = i;
-        LIST_PREPEND(tmp, my_numbers);
+        my_numbers = list_prepend(tmp, my_numbers);
     }
+    printf("prepend: %fs\n", (float)(clock() - t)/CLOCKS_PER_SEC);
     
-    for(size_t current = LIST_ITER(my_numbers); current != 0; LIST_ITER_DOWN(current, node_int, my_numbers))
-        printf("%d\n", LIST_NODE_GET(node_int, current)->data);
+    for(size_t current = LIST_ITER(my_numbers); current != 0;  current = list_iter_down(current))
+        printf("%d\n", LIST_NODE_VAL(node_int, current)->data);
     
-    LIST_DELETE_STATIC(node_int, my_numbers, free);
-
+    list_delete_static(my_numbers, free);
     return 0;
 }
