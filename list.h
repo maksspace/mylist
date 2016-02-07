070112d9e5c34f814e2a4668f663c790fd916bfb
maksspace <maksspaces@mail.ru>
2016-02-07 12:12:52 +0300

    change
    change list_nodes_del
    add method: list_node_init

--- list.h
+++ list.h
@@ -27,34 +27,47 @@
 #ifndef maksspace_list_h
 #define maksspace_list_h
 
-typedef struct { void *prev, *next; } node_link_t;
+/* entry point in node */
+#define LIST_LINK(type) struct type *prev, *next
+
+typedef struct { void *prev, *next; } __node_link_t;
 
 /* this macros through the list starting from the head while maintaining the current node in current */
-#define list_foreach(head, current) \
-    for(typeof(head) current = head; current != NULL; current = current->next)
+#define list_foreach(type, current, head) \
+for(type* current = head; current != NULL; current = current->next)
+
+/* node init */
+void list_node_init(void* node) {
+    *(__node_link_t*)node = (__node_link_t){NULL, NULL};
+}
 
 /* insert node before head, and return pointer on new head of list */
 void* list_prepend(void* node, void* head) {
-    *(node_link_t*)node = (node_link_t){NULL, head};
-    return ((node_link_t*)head)->prev = node;
+    *(__node_link_t*)node = (__node_link_t){NULL, head};
+    return ((__node_link_t*)head)->prev = node;
 }
 
 /* insert node after tail, and return pointer on new tail of list */
 void* list_append(void* tail, void* node) {
-    *(node_link_t*)node = (node_link_t){tail, NULL};
-    return ((node_link_t*)tail)->next = node;
+    *(__node_link_t*)node = (__node_link_t){tail, NULL};
+    return ((__node_link_t*)tail)->next = node;
 }
 
 /* insert new_node between prev and next, return pointer on new_node */
 void* list_insert(void* new_node, void* prev, void* next) {
-    *(node_link_t*)new_node = (node_link_t){prev, next};
-    return ((node_link_t*)prev)->next = ((node_link_t*)next)->prev = new_node;
+    *(__node_link_t*)new_node = (__node_link_t){prev, next};
+    return ((__node_link_t*)prev)->next = ((__node_link_t*)next)->prev = new_node;
 }
 
-/* delete nodes between from and to */
-void list_nodes_del(void* from, void* to) {
-    ((node_link_t*)from)->next = to;
-    ((node_link_t*)to)->prev = from;
+/* delete nodes between `from` and `to`. return a pointer to deleted nodes, or NULL if  betwen `from` and `to` is empty */
+void* list_nodes_del(void* from, void* to) {
+    if(((__node_link_t*)from)->next == to) return NULL;
+    ((__node_link_t*)((__node_link_t*)to)->prev)->next = NULL;
+    ((__node_link_t*)((__node_link_t*)from)->next)->prev = NULL;
+    void* sub_list_ptr = ((__node_link_t*)from)->next;
+    ((__node_link_t*)from)->next = to;
+    ((__node_link_t*)to)->prev = from;
+    return sub_list_ptr;
 }
 
 /* -----------------------------------
@@ -64,13 +77,14 @@ void list_nodes_del(void* from, void* to) {
  * -----------------------------------
  * Example:
  
-   typedef struct your_node_name {
-       struct your_node_name *prev, *next;
-       int my_cool_int;
-       float my_cool_float;
-       ...
-       strcut { ... } name;
-   } some_node_name;
+ typedef struct your_node_name {
+ struct your_node_name *prev, *next;
+ int my_cool_int;
+ float my_cool_float;
+ ...
+ strcut { ... } name;
+ } some_node_name;
  */
 
 #endif /* maksspace_list_h */
+
