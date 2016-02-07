29e3a2e921ba444d80df3fa5755b21498c576370
maksspace <maksspaces@mail.ru>
2016-02-07 11:39:38 +0300

    add license in list.h

--- list.h
+++ list.h
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2016 Maks
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // ---------------------------------------------- //
 // Doubly linked list implementation by maksspace //
 // ---------------------------------------------- //
@@ -7,29 +29,29 @@
 
 typedef struct { void *prev, *next; } node_link_t;
 
-/* this macros through the list starting from the $head while maintaining the current node in $current */
+/* this macros through the list starting from the head while maintaining the current node in current */
 #define list_foreach(head, current) \
     for(typeof(head) current = head; current != NULL; current = current->next)
 
-/* insert $node before $head, and return pointer on new head of list */
+/* insert node before head, and return pointer on new head of list */
 void* list_prepend(void* node, void* head) {
     *(node_link_t*)node = (node_link_t){NULL, head};
     return ((node_link_t*)head)->prev = node;
 }
 
-/* insert $node after $tail, and return pointer on new tail of list */
+/* insert node after tail, and return pointer on new tail of list */
 void* list_append(void* tail, void* node) {
     *(node_link_t*)node = (node_link_t){tail, NULL};
     return ((node_link_t*)tail)->next = node;
 }
 
-/* insert $new_node between $prev and $next, return pointer on $new_node */
+/* insert new_node between prev and next, return pointer on new_node */
 void* list_insert(void* new_node, void* prev, void* next) {
     *(node_link_t*)new_node = (node_link_t){prev, next};
     return ((node_link_t*)prev)->next = ((node_link_t*)next)->prev = new_node;
 }
 
-/* delete nodes between $from and $to */
+/* delete nodes between from and to */
 void list_nodes_del(void* from, void* to) {
     ((node_link_t*)from)->next = to;
     ((node_link_t*)to)->prev = from;
